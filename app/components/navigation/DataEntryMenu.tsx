1f051110d8cadce091360e367152eef95678133b
christina <andrea.christina.unger@gmail.com>
2021-09-16 14:23:12 +0200

    add: separate observation entries for each animal

--- app/components/navigation/DataEntryMenu.tsx
+++ app/components/navigation/DataEntryMenu.tsx
@@ -4,6 +4,8 @@ import Start from '../../screens/Start';
 import BeachClean from '../../screens/BeachClean';
 import Fisheries from '../../screens/Fisheries';
 import Observation from '../../screens/Observation';
+import HumpbackWhale from '../../screens/HumpbackWhale';
+import MantaAndCo from '../../screens/MantaAndCo';
 
 import { createDrawerNavigator } from '@react-navigation/drawer';
 const Drawer = createDrawerNavigator();
@@ -14,7 +16,11 @@ function DataEntryMenu() {
       <Drawer.Screen name='Start' component={Start} />
       <Drawer.Screen name='🗑️ Beach clean' component={BeachClean} />
       <Drawer.Screen name='🎣 Fisheries' component={Fisheries} />
-      <Drawer.Screen name='🐋 Observation' component={Observation} />
+      <Drawer.Screen name='Observation:' component={Observation} />
+      <Drawer.Screen name='🐋 Humpback whale' component={HumpbackWhale} />
+      <Drawer.Screen name='🦈 Whale shark' component={MantaAndCo} />
+      <Drawer.Screen name='🛸 Manta ray' component={MantaAndCo} />
+      <Drawer.Screen name='🐢 Turtle' component={MantaAndCo} />
     </Drawer.Navigator>
   );
 }
--- app/screens/MantaAndCo.tsx
+++ app/screens/MantaAndCo.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
 
-function Observation() {
+function MantaAndCo() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Please open the menu and pick what you saw.</Text>
+      <Text>Yay! Lucky bastard!</Text>
     </View>
   );
 }
 
-export default Observation;
+export default MantaAndCo;
