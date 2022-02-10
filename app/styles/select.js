f6179655f053672afca08f52329de7929a6a9de3
christina <andrea.christina.unger@gmail.com>
2022-02-10 16:08:50 +0100

    don't delete too much...

---
+++ app/styles/select.js
@@ -0,0 +1,13 @@
+const input = {
+  paddingVertical: 8,
+  paddingHorizontal: 8
+};
+
+const inputContainer = {
+  backgroundColor: 'white',
+  borderRadius: 6
+};
+
+module.exports = {
+  input, inputContainer
+}
