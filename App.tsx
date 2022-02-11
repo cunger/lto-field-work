a757da2dc50eaf76f846115f534950b4b65d3ee3
christina <andrea.christina.unger@gmail.com>
2022-02-11 11:59:52 +0100

    fix: add TailwindProvider

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import FlashMessage from 'react-native-flash-message';
 import MainMenu from './app/components/navigation/MainMenu';
 import GlobalContext from './app/components/context/GlobalContext';
+import { TailwindProvider } from 'tailwind-rn';
+import utilities from './tailwind.json';
 
 export default function App() {
   useEffect(() => {
@@ -11,9 +13,11 @@ export default function App() {
   }, []);
 
   return (
-    <NavigationContainer>
-      <MainMenu />
-      <FlashMessage position="top" />
-    </NavigationContainer>
+    <TailwindProvider utilities={utilities}>
+      <NavigationContainer>
+        <MainMenu />
+        <FlashMessage position="top" />
+      </NavigationContainer>
+    </TailwindProvider>
   );
 }
