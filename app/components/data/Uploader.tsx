db535dd7886a95fa0c208d62dcd2d3badef7dd1a
christina <andrea.christina.unger@gmail.com>
2023-03-11 14:57:15 +0100

    revert: replace xhr again by fetch

--- app/components/data/Uploader.tsx
+++ app/components/data/Uploader.tsx
@@ -32,52 +32,47 @@ export default async function upload(items: Item[], i18n: I18n, increaseUploadPr
   for (let item of items) {
     if (!item.photos) continue; 
     
-    for (let image of item.photos) {
-      if (!image.location) continue;
-
-      await uploadImage(image, i18n, increaseUploadProgress, stepPercentage, setUploadStatusText);
-    }
+    const imageUploads = item.photos
+      .filter(image => image.location)
+      .map(image => uploadImage(image, i18n, increaseUploadProgress, stepPercentage, setUploadStatusText));
+    
+    await Promise.allSettled(imageUploads);
   }
 
   // Upload data.
   
-  // (But only if its photos have been uploaded successfully.)
-  items = items.filter(item => item.photos.every(photo => photo.link));
+  let uploaded: string[] = [];
+  let errors: string[] = [];
 
-  let uploaded = [];
-  let errors = [];
+  try {
+    setUploadStatusText('/data: ' + items.length + ' entries');
 
-  if (items.length > 0) {
-    try {
-      setUploadStatusText('/data ... ' + items.length + ' entries');
+    const response = await fetch(`${BASE_URL}/data`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'X-Ship-Name': 'BeanWithBaconMegaRocket'
+      },
+      body: JSON.stringify({ items: items.map(item => withPrettyPrintedValues(item, i18n)) })
+    });
 
-      const response = await fetch(`${BASE_URL}/data`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-          'X-Ship-Name': 'BeanWithBaconMegaRocket'
-        },
-        body: JSON.stringify({ items: items.map(item => withPrettyPrintedValues(item, i18n)) })
-      });
+    const responseData = await response.json(); // { uploaded: [], errors: [] }
 
-      const responseData = await response.json(); // { uploaded: [], errors: [] }
+    uploaded = responseData.uploaded || [];
+    errors = responseData.errors || [];
+  } catch (error) {
+    errors.push(error.message);
+  }
 
-      uploaded = responseData.uploaded || [];
-      errors = responseData.errors || [];
-    } catch (error) {
-      errors.push(error.message);
-    }
-
-    if (errors.length > 0) {
-      showMessage({
-        message: i18n.t('ERROR_UPLOAD'),
-        description: errors.join(' | '),
-        type: 'danger',
-        icon: 'danger',
-        duration: 6000
-      });
-    }
+  if (errors.length > 0) {
+    showMessage({
+      message: i18n.t('ERROR_DATA_UPLOAD'),
+      description: errors.join(' | '),
+      type: 'danger',
+      icon: 'danger',
+      duration: 6000
+    });
   }
 
   increaseUploadProgress(stepPercentage);
@@ -86,77 +81,51 @@ export default async function upload(items: Item[], i18n: I18n, increaseUploadPr
   return items.filter(item => uploaded.indexOf(item.id) >= 0);
 }
 
-function uploadImage(image: Image, i18n: I18n, increaseUploadProgress: (_ : number) => void, stepPercentage: number, setUploadStatusText: (_: string) => void): Promise<void> {
-  return new Promise<void>((resolve, reject) => {
-    try {
-      const formData = new FormData();
-      formData.append('file', {
-        uri: image.location,
-        name: image.filename,
-        type: image.mimetype
-      });
+async function uploadImage(image: Image, i18n: I18n, increaseUploadProgress: (_ : number) => void, stepPercentage: number, setUploadStatusText: (_: string) => void) {
+  const errors: string[] = [];
 
-      const xhr = new XMLHttpRequest();
-      xhr.open('POST', `${BASE_URL}/photo`);
+  try {
+    setUploadStatusText('/photo: ' + image.filename);
 
-      if (xhr.upload) {
-        xhr.upload.onprogress = (event: ProgressEvent) => {
-          setUploadStatusText('/photo ...' + image.filename + ' (' + event.loaded + ' / ' + event.total + ')');
-        };
-      }
+    const formData = new FormData();
+    formData.append('file', {
+      uri: image.location,
+      name: image.filename,
+      type: image.mimetype
+    });
 
-      xhr.onload = () => {
-        const responseData = JSON.parse(xhr.response || xhr.responseText); // { link: '', errors: [] }
-        image.link = responseData.link;
-        increaseUploadProgress(stepPercentage);
-        setUploadStatusText('Done.');
-        resolve();
-      };
+    const response = await fetch(`${BASE_URL}/photo`, {
+      method: 'POST',
+      headers: {
+        // Let fetch set the content type header, because it knows the boundary string.
+        // 'Content-Type': 'multipart/form-data',
+        'Accept': 'application/json',
+        'X-Ship-Name': 'BeanWithBaconMegaRocket'
+      },
+      body: formData
+    });
 
-      xhr.onerror = () => {
-        showMessage({
-          message: i18n.t('ERROR_UPLOAD'),
-          description: xhr.responseText,
-          type: 'danger',
-          icon: 'danger',
-          duration: 6000
-        });
-        reject();
-      };
-
-      xhr.ontimeout = () => {
-        // Image was not uploaded, so the item is not uploaded and marked as sync.
-        // The user can just try to upload again later.
-        setUploadStatusText('Timeout.')
-        reject();
-      }
-
-      xhr.onabort = () => {
-        // Image was not uploaded, so the item is not uploaded and marked as sync.
-        // The user can just try to upload again later.
-        setUploadStatusText('Abort.')
-        reject();
-      };
-
-      // The 'Content-Type': 'multipart/form-data' header should be set automatically, 
-      // because it needs a boundary string.
-      xhr.setRequestHeader('Accept', 'application/json');
-      xhr.setRequestHeader('X-Ship-Name', 'BeanWithBaconMegaRocket');
-      
-      xhr.send(formData);
-    } catch (error) {
-      showMessage({
-        message: i18n.t('ERROR_UPLOAD'),
-        description: `${error}`,
-        type: 'danger',
-        icon: 'danger',
-        duration: 6000
-      });
-
-      setUploadStatusText('Error.')
-      reject();
+    if (response.status === 200) {
+      const responseData = await response.json(); // { link: '', errors: [] }
+      image.link = responseData.link;
+    } else {
+      errors.push(`Response status: ${response.status}`);
     }
-  });
+
+    increaseUploadProgress(stepPercentage);
+  } catch (error) {
+    errors.push(`${error}`);
+  }
+
+  if (errors.length > 0) {
+    showMessage({
+      message: i18n.t('ERROR_IMAGE_UPLOAD'),
+      description: errors.join(' | '),
+      type: 'danger',
+      icon: 'danger',
+      duration: 4000
+    });
+  }
 }
 
 function withPrettyPrintedValues(item: Item, i18n: I18n) {
